5aff34ab3b5348f3bef80e80f991a66d4cb43251
Tiago M. Vieira <tiago@tvieira.com>
2024-05-02 14:39:59 -0300

    add unit test

--- src/myapp/tests.py
+++ src/myapp/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class ViewsTestCase(TestCase):
+    def test_index_loads_properly(self):
+        """The index page loads properly"""
+        response = self.client.get("127.0.0.1:8000")
+        self.assertEqual(response.status_code, 404)
