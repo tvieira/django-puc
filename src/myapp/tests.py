6720627293beec8ba9192b0fb25643b5312500c0
Tiago M. Vieira <tiago@tvieira.com>
2024-05-02 21:13:44 -0300

    correcao de teste

--- src/myapp/tests.py
+++ src/myapp/tests.py
@@ -9,4 +9,4 @@ class ViewsTestCase(TestCase):
     def test_index_loads_properly(self):
         """testa se index roda"""
         response = self.client.get("127.0.0.1:8000")
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 404)
